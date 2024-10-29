24fb7ab4589867ec02947bda8b49bccbafefbba1
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-10-29 06:50:32 +0530

    Add files via upload

---
+++ JS_pratice/script.js
@@ -0,0 +1,16 @@
+let a = 10;
+console.log(10)
+console.log(`Hello`)
+console.log(`Welcome to java script`)
+
+// window.alert(`this is an alert`)
+
+// This is an comment
+/*
+this 
+is an 
+comment
+*/
+
+document.getElementById("1").textContent='This is id';
+document.getElementById("2").textContent="hai";
---
+++ JS_pratice/variable.js
@@ -0,0 +1,28 @@
+let x;
+x = 100;
+console.log("100")
+let age = 19;
+console.log("age");
+
+let price = 10.99;
+console.log(price);
+
+console.log(`you are ${age} years old`) //you are 19 years old
+let name = 'String1'
+let name1 = "String2"
+console.log(typeof name); // string type
+
+console.log(`price of this ${price} channel`)
+//String
+let email = `dk@gmail.com`
+console.log(`this is my ${email} emailid`)
+
+// boolean
+let online = false;
+console.log(`dk is online ${online}`)
+
+let fullName = "Dhinesh Kumar"
+let age1 = 19;
+
+document.getElementById("p1").textContent= `my name is ${fullName}`;
+document.getElementById("p2").textContent=age1
