a8952ffe1546796350b82c5391e9ecf59c72c029
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-10-29 07:04:45 +0530

    Add files via upload

---
+++ JS_pratice/arithmetic.js
@@ -0,0 +1,44 @@
+// let a  =10;
+// console.log(10+10);
+
+
+//Aritmetic operator
+let std = 30;
+// console.log(std);
+// std = std + 1;
+// console.log(std);
+// std  =std -1;
+// console.log(std);
+// std = std * 2;
+// console.log(std);
+// std = std / 2;
+// console.log(std);
+// std  =std % 2;
+// console.log(std);
+
+// other type
+// std +=2;
+// console.log(std);
+// std -=2;
+// console.log(std);
+// std *=2;
+// console.log(std);
+// std /=2;
+// console.log(std);
+
+
+// Increment or decrement
+// std++;
+// console.log(std)
+// std--;
+// console.log(std)
+
+// operator precendence
+let res = 1 + 2 * 3 + 4 ** 2;
+console.log(res) //23
+
+let result = 12%5+8/2;
+console.log(result) //6
+
+let r = 6/2**7;
+console.log(r); //0.046875
